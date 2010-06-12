887f395def15626eb3bedac811610d226d9c4626
Jon Bringhurst <jon@bringhurst.org>
2010-06-12 19:04:45 -0400

    Minor jsdoc change.

--- src/util/Color.js
+++ src/util/Color.js
@@ -318,7 +318,7 @@ lanyard.util.Color.prototype.getAlpha = function () {
  * Return the sRGB value of this color. Bits 24-31 are alpha, 16-23 are red,
  * 8-15 are green, 0-7 are blue. Since javascript operands are converted from
  * floating point to integers and back when bitwise operations are used, the
- * results from this function may not give consistant results.
+ * results from this function may not be consistant.
  *
  * @return {number} the sRGB value of this color.
  */
