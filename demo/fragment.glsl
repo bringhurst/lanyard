468cbd2677248a0be3db74c9703df6c67c3c668c
Jon Bringhurst <jon@bringhurst.org>
2010-06-17 16:40:31 -0400

    Put texture coordinates into a proper array.

--- demo/fragment.glsl
+++ demo/fragment.glsl
@@ -4,16 +4,16 @@ uniform sampler2D uSamplerAlphaMask;
 uniform int showOutlines;
 
 void main (void) {
-    vec4 tileColor = texture2D(uSamplerTileImage, vec2(vTextureCoord0.s, vTextureCoord0.t));
-    vec4 alphaColor = texture2D(uSamplerAlphaMask, vec2(vTextureCoord1.s, vTextureCoord1.t));
+    vec4 tileColor = texture2D(uSamplerTileImage, vec2(vTextureCoord[0].s, vTextureCoord[0].t));
+    vec4 alphaColor = texture2D(uSamplerAlphaMask, vec2(vTextureCoord[1].s, vTextureCoord[1].t));
 
     if(showOutlines == 1) {
         // Black borders 50 percent.
-        if(vTextureCoord0.s < 0.01 || vTextureCoord0.s > 0.99) {
+        if(vTextureCoord[0].s < 0.01 || vTextureCoord[0].s > 0.99) {
             tileColor = mix(tileColor, vec4(0.0, 0.0, 0.0, tileColor.a), 0.5);
         }
 
-        if(vTextureCoord0.t < 0.01 || vTextureCoord0.t > 0.99) {
+        if(vTextureCoord[0].t < 0.01 || vTextureCoord[0].t > 0.99) {
             tileColor = mix(tileColor, vec4(0.0, 0.0, 0.0, tileColor.a), 0.5);
         }
     }
--- demo/vertex.glsl
+++ demo/vertex.glsl
@@ -1,17 +1,15 @@
-attribute vec2 aTextureCoord0;
-attribute vec2 aTextureCoord1;
+attribute vec2 aTextureCoord[2];
 attribute vec3 aVertexPosition;
 
 uniform mat4 uMVMatrix;
 uniform mat4 uPMatrix;
 uniform mat4 uTextureMatrix[2];
 
-varying vec2 vTextureCoord0;
-varying vec2 vTextureCoord1;
+varying vec2 vTextureCoord[2];
 
 void main(void) {
-    vTextureCoord0 = uTextureMatrix[0] * aTextureCoord0;
-    vTextureCoord1 = uTextureMatrix[0] * aTextureCoord1;
+    vTextureCoord[0] = uTextureMatrix[0] * aTextureCoord[0];
+    vTextureCoord[1] = uTextureMatrix[0] * aTextureCoord[1];
 
     gl_Position = uPMatrix * uMVMatrix * vec4(aVertexPosition, 1.0);
 }
