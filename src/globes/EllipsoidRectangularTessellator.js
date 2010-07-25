38928d6fa79c003fa6261232c2ccd02b5fe61c0d
Jon Bringhurst <jon@bringhurst.org>
2010-07-25 19:12:05 -0400

    More debug messages.

--- src/globes/EllipsoidRectangularTessellator.js
+++ src/globes/EllipsoidRectangularTessellator.js
@@ -230,6 +230,12 @@ lanyard.globes.EllipsoidRectangularTessellator.prototype.selectVisibleTiles = fu
  */
 lanyard.globes.EllipsoidRectangularTessellator.prototype.needToSplit = function (dc, tile) {
 
+    if(!tile || !(tile.getSector())) {
+        this._logger.error("Attempted to perform a split check on an invalid tile.");
+    } else {
+        this._logger.fine("Split check with sector: " + tile.getSector().toString());
+    }
+
     /** @type {Array.<lanyard.geom.Point>} */
     var corners = tile.getSector().computeCornerPoints(dc.getGlobe());
 
