1f2cffdd718d9527dbbfd2d50dd29605a3ab9778
jonb <jonb@lanl.gov>
2012-04-21 11:19:50 -0600

    Fixed animation loop to actually use requestAnimationFrame if available.

--- src/demo/BasicDemo.js
+++ src/demo/BasicDemo.js
@@ -106,23 +106,24 @@ lanyard.demo.BasicDemo.prototype.run = function() {
   /** @type {lanyard.demo.BasicDemo} */
   var self = this;
 
-  if (window['requestAnimationFrame']) {
-    this._logger.fine('Using animation frame requests for rendering.');
-    function step(event) {
-      self.lanyardCanvas.display();
-      window['requestAnimationFrame']();
-    }
-    window.addEventListener('BeforePaint', step, false);
-    window['requestAnimationFrame']();
-  } else {
-    this._logger.fine('Using setTimeout for rendering.');
-    (function loop() {
-      window.setTimeout(function() {
-        self.lanyardCanvas.display();
-        loop();
-      }, 10);
-    })();
-  }
+  var requestAnimFrame = function(){
+    return (
+      window.requestAnimationFrame       || 
+      window.webkitRequestAnimationFrame || 
+      window.mozRequestAnimationFrame    || 
+      window.oRequestAnimationFrame      || 
+      window.msRequestAnimationFrame     || 
+
+      function(callback) {
+        window.setTimeout(callback, 1000 / 60);
+      }
+    );
+  }();
+
+  (function step() {
+    self.lanyardCanvas.display();
+    requestAnimFrame(step);
+  }());
 };
 goog.exportSymbol('lanyard.demo.BasicDemo.prototype.run',
     lanyard.demo.BasicDemo.prototype.run);
