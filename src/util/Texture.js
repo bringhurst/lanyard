e07773f9fd50b4546aed4ea1e5458d667ad0bbcd
Jon Bringhurst <jon@bringhurst.org>
2013-11-09 16:22:40 -0700

    Fix type of parameter.

--- src/util/Texture.js
+++ src/util/Texture.js
@@ -82,7 +82,7 @@ lanyard.util.Texture.prototype.updateCanvas = function(textureCanvas) {
     var gl = self.gl;
 
     gl.bindTexture(gl.TEXTURE_2D, this.tex);
-    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
